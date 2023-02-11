d3cc065f3e4028d0a2fec3a5e55d079171b7fb66
diecknet <54552094+diecknet@users.noreply.github.com>
2023-02-11 12:50:03 +0100

    move WifiCredentials.h to include folder

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <./WiFiCredentials.h>
+#include <WiFiCredentials.h>
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
